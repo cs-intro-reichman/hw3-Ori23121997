5c28e3047a50799fb8479bab0f8fc9ec8f917385
Ori Gerstein <ori.gerstein@post.runi.ac.il>
2025-11-17 11:45:26 +0200

    Completed assignment

--- Algebra.java
+++ Algebra.java
@@ -24,44 +24,131 @@ public class Algebra {
 	}  
 
 	// Returns x1 + x2
+	// if a or b =0 return the x or b
+	// we need to count i a times
+	// "          " b times
+	// need i will be i>=a and i >=b
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int i =0;
+		int j =0;
+		if (x2 == 0 ){
+			return x1;
+		}else if (x1 == 0 ){
+			return x2;
+		}
+		else { while (i<x1){
+			i++;
+		} while (j <x2 ) {
+			i++;
+			j++;
+		}return i;
 	}
+}
+
+	
 
 	// Returns x1 - x2
+
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int i =0;
+		int j =0;
+		if (x2 == 0 ){
+			return x1;
+		}else if (x1 == 0 ){
+			return -x2;
+		}
+		else { while (i<x1){
+			i++;
+		} while (j <x2 ) {
+			i--;
+			j++;
+		}return i;
+	}
 	}
 
 	// Returns x1 * x2
+	// we need to use plus
+	//if x1 or x2 == 0 ret 0
+	// if x1 xor x2 negative 
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
-
+		int i = 0;
+		int j = 0;
+		if (x1 == 0 || x2 == 0)
+			return 0;
+		else if (x1 < 0 || x2 < 0){
+			if (x2 <0) {
+			}x2 = -x2;
+			while ( i < x2 ){
+				j=plus(j, x1);
+				i++;
+			}return -j;
+		}else if (!(x1 < 0 ^ x2 < 0)){
+			while ( i<x2 ){
+				j=plus(j, x1);
+				i++;
+		}
+	}return j;
+}
 	// Returns x^n (for n >= 0)
+	// x*x n times5
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int j = 1;
+		if (n == 0){
+			return 1;
+		}else {for (int i = 0; i < n; i++){
+			j = times(j, x);
+			
+		}
+	}
+		return j;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int i = 0 ;
+		int j = 0;
+		if (x1 == 0) {
+			return 0;
+		}
+		if (x2 == 0){
+			return -1;
+		}else if (x1 < x2) {
+			return 0;
+		}else {while (j<x1) {
+			j = plus(j, x2);
+			i++;
+		} 
+		if (times(i, x2) > x1) {
+			return --i;
+		}return i;
 	}
+}
+	
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}	
+		if (x1 == 0) {
+			return 0;
+		}
+		if (x2 == 0){
+			return -1;
+		}else if (x1 < x2) {
+			return x1;
+	}	return minus(x1 , times(div(x1,x2), x2));
+}
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int i = 2;
+		if (x == 1 || x == 0) {
+			return x;
+		}else { while (pow(i, 2)<x){
+			i++;
+		}
+		if (pow(i, 2) == x) {
+			return i;
+		}
+		}
+		return--i;
 	}	  	  
 }
